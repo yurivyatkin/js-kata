e9ea9dee1395ae6afa5f0d9d3acb5f4ea3cb7ad9
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2018-07-01 00:16:40 +1200

    Rename the main function ot fizzBuzz

--- kata/fizzBuzz/fizzBuzz.test.js
+++ kata/fizzBuzz/fizzBuzz.test.js
@@ -1,20 +1,20 @@
-const { fizzBuzzTest } = require('./fizzBuzz');
+const { fizzBuzz } = require('./fizzBuzz');
 
 describe('fizzBuzzTest', () => {
   it('given a multiple of 3 but not a multiple of 5, returns "Fizz"', () => {
-    const result = fizzBuzzTest(6);
+    const result = fizzBuzz(6);
     expect(result).toEqual('Fizz');
   });
   it('given a multiple of 5 but not a multiple of 3, returns "Buzz"', () => {
-    const result = fizzBuzzTest(10);
+    const result = fizzBuzz(10);
     expect(result).toEqual('Buzz');
   });
   it('given a multiple of 3 which is also a multiple of 5, returns "FizzBuzz"', () => {
-    const result = fizzBuzzTest(15);
+    const result = fizzBuzz(15);
     expect(result).toEqual('FizzBuzz');
   });
   it('given a not a multiple of 3 or 5, returns the input', () => {
-    const result = fizzBuzzTest(4);
+    const result = fizzBuzz(4);
     expect(result).toEqual(4);
   });
 });
