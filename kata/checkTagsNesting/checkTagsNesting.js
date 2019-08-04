3da29fe4dac4270ab4c7cad371e33bda4946d37b
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2019-08-04 22:09:29 +1200

    Implement the main method

--- kata/checkTagsNesting/checkTagsNesting.js
+++ kata/checkTagsNesting/checkTagsNesting.js
@@ -1,4 +1,17 @@
-const checkTagsNesting = () => {};
+const { findMismatch } = require('./findMismatch');
+const { getTagTokens } = require('./getTagTokens');
+
+const getTag = tagString => (tagString === '#' ? tagString : `</${tagString}>`);
+
+const checkTagsNesting = taggedString => {
+  const mismatch = findMismatch(getTagTokens(taggedString));
+  if (mismatch) {
+    const expected = getTag(mismatch.expected);
+    const found = getTag(mismatch.found);
+    return `Expected ${expected} found ${found}`;
+  }
+  return 'Correctly tagged paragraph';
+};
 
 module.exports = {
   checkTagsNesting,
