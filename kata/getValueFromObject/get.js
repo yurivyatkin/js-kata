d5c3b12834762fe87e1607f540dba786ff7acea4
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-08-08 15:50:24 +1200

    Simplify the solution - no isObject needed! Borrowed from what Timur Gafiulin @TatarinFrontend come up with during the interview.

--- kata/getValueFromObject/get.js
+++ kata/getValueFromObject/get.js
@@ -1,21 +1,10 @@
-// https://stackoverflow.com/a/25715455/1008341
-function isObject(item) {
-  return typeof item === 'object' && !Array.isArray(item) && item !== null;
-}
-
 const get = (keySequence, nestedObject) => {
-  if (keySequence === '') {
-    return nestedObject;
-  }
-  if (!isObject(nestedObject)) {
-    return undefined;
-  }
   const keys = keySequence.split('.');
   let currentKey = keys.shift();
-  let currentValue = nestedObject[currentKey];
-  while (currentKey && currentValue && isObject(currentValue)) {
-    currentKey = keys.shift();
+  let currentValue = nestedObject;
+  while (currentKey && currentValue) {
     currentValue = currentValue[currentKey];
+    currentKey = keys.shift();
   }
   return currentValue;
 };
