56500c39fb56b34bc5027593468fe24614b9418d
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2022-01-08 22:00:36 +1300

    Steal a clever solution after sumbitting No reduce, pure recursion. It flattens by destructuring, but uses memory to store depths for non-arrays too.

--- kata/depthOfArray/depthOfArray.js
+++ kata/depthOfArray/depthOfArray.js
@@ -2,11 +2,6 @@
  * Calculates the depth of an array.
  * @param {Array} arr
  */
-function depth(arr) {
-  return arr.reduce(
-    (d, v) => (Array.isArray(v) ? Math.max(d, depth(v) + 1) : d),
-    1
-  );
-}
+const depth = arr => (Array.isArray(arr) ? 1 + Math.max(...arr.map(depth)) : 0);
 
 module.exports = { depth };
