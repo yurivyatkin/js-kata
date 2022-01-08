5078d873ae665f7d603c26da9e2abe2496527d8e
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2022-01-08 21:37:34 +1300

    Let Copilot implement depthOfArray

--- kata/depthOfArray/depthOfArray.js
+++ kata/depthOfArray/depthOfArray.js
@@ -1,7 +1,15 @@
 /**
+ * Calculates the depth of an array.
  * @param {Array} arr
  */
-// eslint-disable-next-line no-unused-vars
-function depth(arr) {}
+function depth(arr) {
+  let d = 1;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (Array.isArray(arr[i])) {
+      d = Math.max(d, depth(arr[i]) + 1);
+    }
+  }
+  return d;
+}
 
 module.exports = { depth };
--- kata/depthOfArray/depthOfArray.test.js
+++ kata/depthOfArray/depthOfArray.test.js
@@ -6,31 +6,31 @@ const { depth } = require('./depthOfArray');
 // Test.assertEquals(depth([1, [2, 3, 4]]), 2)
 // Test.assertEquals(depth([1, [2, [3, 4]]]), 3)
 // Test.assertEquals(depth([1, [2, [3, [4]]]]), 4)
-
+//
 // Test.assertEquals(depth([1, [2, [3, [4]]], 4]), 4)
 // Test.assertEquals(depth([1, [2], 3, [4], 5, [6]]), 2)
 // Test.assertEquals(depth([1, 2, 3, 4, [[5]], [6], 7]), 3)
 
 describe('depthOfArray', () => {
-  xit('returns 1 for [1, 2, 3, 4]', () => {
+  it('returns 1 for [1, 2, 3, 4]', () => {
     expect(depth([1, 2, 3, 4])).toBe(1);
   });
-  xit('returns 2 for [1, [2, 3, 4]]', () => {
+  it('returns 2 for [1, [2, 3, 4]]', () => {
     expect(depth([1, [2, 3, 4]])).toBe(2);
   });
-  xit('returns 3 for [1, [2, [3, 4]]]', () => {
+  it('returns 3 for [1, [2, [3, 4]]]', () => {
     expect(depth([1, [2, [3, 4]]])).toBe(3);
   });
-  xit('returns 4 for [1, [2, [3, [4]]]]', () => {
+  it('returns 4 for [1, [2, [3, [4]]]]', () => {
     expect(depth([1, [2, [3, [4]]]])).toBe(4);
   });
-  xit('returns 4 for [1, [2, [3, [4]]], 4]', () => {
+  it('returns 4 for [1, [2, [3, [4]]], 4]', () => {
     expect(depth([1, [2, [3, [4]]], 4])).toBe(4);
   });
-  xit('returns 2 for [1, [2], 3, [4], 5, [6]]', () => {
+  it('returns 2 for [1, [2], 3, [4], 5, [6]]', () => {
     expect(depth([1, [2], 3, [4], 5, [6]])).toBe(2);
   });
-  xit('returns 3 for [1, 2, 3, 4, [[5]], [6], 7]', () => {
+  it('returns 3 for [1, 2, 3, 4, [[5]], [6], 7]', () => {
     expect(depth([1, 2, 3, 4, [[5]], [6], 7])).toBe(3);
   });
 });
