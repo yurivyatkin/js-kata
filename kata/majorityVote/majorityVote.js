a717e579c62ed8d93c78754fddd0553a3723c619
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2022-01-09 23:50:30 +1300

    Let Copilot solve the problem (only had to nudge it to check > half the length)

--- kata/majorityVote/majorityVote.js
+++ kata/majorityVote/majorityVote.js
@@ -3,7 +3,23 @@
  * @param {Array} arr
  * @returns {String || null}
  */
-// eslint-disable-next-line no-unused-vars
-function majorityVote(arr) {}
+function majorityVote(arr) {
+  const counts = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (counts[arr[i]] === undefined) {
+      counts[arr[i]] = 1;
+    } else {
+      counts[arr[i]]++;
+    }
+  }
+  const max = Math.max(...Object.values(counts));
+  const keys = Object.keys(counts);
+  for (let i = 0; i < keys.length; i++) {
+    if (counts[keys[i]] === max && max > Math.floor(arr.length / 2)) {
+      return keys[i];
+    }
+  }
+  return null;
+}
 
 module.exports = { majorityVote };
--- kata/majorityVote/majorityVote.test.js
+++ kata/majorityVote/majorityVote.test.js
@@ -14,34 +14,34 @@ const { majorityVote } = require('./majorityVote');
 // Test.assertEquals(majorityVote([]), null)
 
 describe('majorityVote', () => {
-  xit('returns null for ["A", "B", "B", "B", "A", "A"]', () => {
+  it('returns null for ["A", "B", "B", "B", "A", "A"]', () => {
     expect(majorityVote(['A', 'B', 'B', 'B', 'A', 'A'])).toBe(null);
   });
-  xit('returns "B" for ["B", "B", "B"]', () => {
+  it('returns "B" for ["B", "B", "B"]', () => {
     expect(majorityVote(['B', 'B', 'B'])).toBe('B');
   });
-  xit('returns "B" for ["A", "B", "B"]', () => {
+  it('returns "B" for ["A", "B", "B"]', () => {
     expect(majorityVote(['A', 'B', 'B'])).toBe('B');
   });
-  xit('returns "A" for ["A", "A", "B"]', () => {
+  it('returns "A" for ["A", "A", "B"]', () => {
     expect(majorityVote(['A', 'A', 'B'])).toBe('A');
   });
-  xit('returns "A" for ["A", "A", "A", "B", "C", "A"]', () => {
+  it('returns "A" for ["A", "A", "A", "B", "C", "A"]', () => {
     expect(majorityVote(['A', 'A', 'A', 'B', 'C', 'A'])).toBe('A');
   });
-  xit('returns "B" for ["B", "A", "B", "B", "C", "A", "B", "B"]', () => {
+  it('returns "B" for ["B", "A", "B", "B", "C", "A", "B", "B"]', () => {
     expect(majorityVote(['B', 'A', 'B', 'B', 'C', 'A', 'B', 'B'])).toBe('B');
   });
-  xit('returns null for ["A", "B", "B", "A", "C", "C"]', () => {
+  it('returns null for ["A", "B", "B", "A", "C", "C"]', () => {
     expect(majorityVote(['A', 'B', 'B', 'A', 'C', 'C'])).toBe(null);
   });
-  xit('returns null for ["A", "B"]', () => {
+  it('returns null for ["A", "B"]', () => {
     expect(majorityVote(['A', 'B'])).toBe(null);
   });
-  xit('returns "A" for ["A"]', () => {
+  it('returns "A" for ["A"]', () => {
     expect(majorityVote(['A'])).toBe('A');
   });
-  xit('returns null for []', () => {
+  it('returns null for []', () => {
     expect(majorityVote([])).toBe(null);
   });
 });
