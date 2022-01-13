00c14dfe19d1c60f1dd5102cf6a627c294466581
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2022-01-13 23:35:56 +1300

    Find a solution inspired by the most upvoded solutions on Edabit.

--- kata/rotateMatrixClockwise/rotateMatrixClockwise.js
+++ kata/rotateMatrixClockwise/rotateMatrixClockwise.js
@@ -2,20 +2,11 @@
  * Rotates a matrix 90 degrees clockwise
  * @param {any[][]} arr
  */
-// eslint-disable-next-line no-unused-vars
 function rotate(arr) {
-  // Determine the size of the matrix:
-  const m = arr.length; // assuming it is an array
-  const n = arr[0].length; // assuming that all the elements are arrays of the same length
-  const result = Array(m)
-    .fill()
-    .map(() => Array(n));
-  for (let i = 0; i < m; i += 1) {
-    for (let j = 0; j < n; j += 1) {
-      result[i][j] = arr[n - j - 1][i];
-    }
-  }
-  return result;
+  return arr
+    .slice()
+    .reverse()
+    .map((_, colIndex) => arr.map(row => row[colIndex]).reverse());
 }
 
 module.exports = { rotate };
