458cd42bbd491c69e500be240ec74ce3d907ee8e
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2022-01-13 22:31:00 +1300

    Implement rotate

--- kata/rotateMatrixClockwise/rotateMatrixClockwise.js
+++ kata/rotateMatrixClockwise/rotateMatrixClockwise.js
@@ -3,6 +3,19 @@
  * @param {any[][]} arr
  */
 // eslint-disable-next-line no-unused-vars
-function rotate(arr) {}
+function rotate(arr) {
+  // Determine the size of the matrix:
+  const m = arr.length; // assuming it is an array
+  const n = arr[0].length; // assuming that all the elements are arrays of the same length
+  const result = Array(m)
+    .fill()
+    .map(() => Array(n));
+  for (let i = 0; i < m; i += 1) {
+    for (let j = 0; j < n; j += 1) {
+      result[i][j] = arr[n - j - 1][i];
+    }
+  }
+  return result;
+}
 
 module.exports = { rotate };
